8d41ee03ff8eabca516753a1973fcb6367815ca3
Vasily Pashchenko <vasiliy.pashchenko@gmail.com>
2019-05-06 14:50:34 +0300

    Added edo files handling.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='SchemaChecker',
-    version='0.0.1b4',
-    packages=['fns', ],
+    version='0.0.2b1',
+    packages=['fns', 'stat', ],
     install_requires=open('requirements.txt').read().splitlines(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
