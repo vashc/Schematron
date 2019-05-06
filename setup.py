db5ff325ed332ee1ff7f16861ca8df87139293a8
Vasily Pashchenko <vasiliy.pashchenko@gmail.com>
2019-05-06 15:34:22 +0300

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from distutils.core import setup
 
 setup(
     name='SchemaChecker',
-    version='0.0.2b2',
-    packages=['schematron'],
+    version='0.0.3.dev2',
+    packages=['schematron',
+              'schematron.edo',
+              'schematron.fns',
+              'schematron.stat',
+              'schematron.pfr'],
     install_requires=open('requirements.txt').read().splitlines(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
