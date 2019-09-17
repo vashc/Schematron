cfcc8b3c8dc6cdfc5a65b335957d8979148227a9
Vasily Pashchenko <vasiliy.pashchenko@gmail.com>
2019-09-17 10:22:49 +0300

    Version fix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SchemaChecker',
-    version='0.0.3.dev37',
+    version='0.0.3.dev38',
     packages=['schematron',
               'schematron.edo',
               'schematron.fns',
